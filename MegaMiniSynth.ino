1fc12a2713d06dc102d7cc184f282730484cf7a7
D. Quentin McDonald <dqmcdonald@gmail.com>
2014-08-01 16:12:54 +1200

    Moved Oscillator code to it's own class

--- MegaMiniSynth.ino
+++ MegaMiniSynth.ino
@@ -1,72 +1,67 @@
 /***************************************************************************
-Mega Mini Synth
-
-An Arduino Mega2560 based music synthesizer
-
-See http://megaminisynth.blogspot.co.nz/ for details.
-
-
-
-****************************************************************************/
-
-// DCO. Based on code from http://www.sinneb.net/?p=161
+ * Mega Mini Synth
+ * 
+ * An Arduino Mega2560 based music synthesizer
+ * 
+ * See http://megaminisynth.blogspot.co.nz/ for details.
+ * 
+ * This file is the main routine for the synth - all command and control operations
+ * are performed from here.
+ * 
+ * D. Quentin McDonald
+ * dqmcdonald@gmail.com
+ * 2014
+ * 
+ ****************************************************************************/
 
 #include <MIDI.h>
 
 
 MIDI_CREATE_INSTANCE(HardwareSerial, Serial1, MIDI);
 
-// frequencies for midi notes 0 - 127
-int musical_freqs[] = { 
-  15289, 14431, 13621, 12857, 12135, 11454, 10811, 10204, 9631, 9091, 8581, 64792, 61156,
-  57724, 54484, 51426, 48540, 45815, 43244, 40817, 38526, 36364, 34323, 32396, 30578, 28862,
-  27242, 25713, 24270, 22908, 21622, 20408, 19263, 18182, 17161, 16198, 15289, 14431, 13621,
-  12856, 12135, 11454, 10811, 10204, 9631, 9091, 8581, 64793, 61156, 57724, 54484, 51426, 48540,
-  45815, 43244, 40817, 38526, 36364, 34323, 32396, 30578, 28862, 27242, 25713, 24270, 22908, 21622,
-  20408, 19263, 18182, 17161, 16198, 15289, 14431, 13621, 12856, 12135, 11454, 10811, 10204, 9631, 9091,
-  8581, 8099, 7645, 7215, 6810, 6428, 6067, 5727, 5405, 5102, 4816, 4545, 4290, 4050, 3822, 3608, 3405,
-  3214, 3034, 2863, 2703, 2551, 2408, 2273, 2145, 2025, 1911, 1804, 1703, 1607, 1517, 1432, 1351, 1276,
-  1204, 1136, 1073, 1012, 956, 902, 851, 804, 758, 716, 676, 638 };
+// Pin definitions:
+
+#define TIMER1_OUT     11
 
 
-int current_note = 69;
-int current_pitch_bend = 0;
 
 
+
+
+
+class Oscillator {
+
+public:
+  //
+  Oscillator( int timer );        // Create an instance of an Oscillator using Timer "timer"
+  void setNote( int current_note );     // Set the current MIDI note
+  void setPitchBend( int pitch_bend ); // Set the Pitchbend amount
+
+protected:
+  void setPreScaling( int midi_note );// Set up the pre-scaler appropriate for the given note
+
+
+  int m_note;
+  int m_pitch_bend;
+  int m_timer;
+};
+
+
+
+
+// Create Oscillator instances
+Oscillator OSC1(1);
 
 
 
 void setup() {
-  pinMode(11, OUTPUT);
 
-  // Timer/Counter 1 :: registers TCCR1A and TCCR1B
-
-  // Clear Timer on Compare Match (CTC) Mode
-  bitWrite(TCCR1A, WGM10, 0);
-  bitWrite(TCCR1A, WGM11, 0);
-  bitWrite(TCCR1B, WGM12, 1);
-  bitWrite(TCCR1B, WGM13, 0);
-
-  // Toggle OC1A on Compare Match. p.134
-  // arduino mega hw pin 11
-  bitWrite(TCCR1A, COM1A0, 1);
-  bitWrite(TCCR1A, COM1A1, 0);
-
-  // prescaling
-  set_prescaling(current_note);
-
-  // set output compare register A to curr_midinote (init: 69)
-  OCR1A = musical_freqs[current_note];
-
-  pinMode(A0,INPUT);
 
   Serial.begin(9600);
 
-  pinMode( 13, OUTPUT);
-  digitalWrite( 13, LOW );
 
 
-  // Connect the callbacks:
+  // Connect the callbacks for MIDI input - these will be called on certain MIDI events:
   // 
   MIDI.setHandleNoteOn(handleNoteOn);  
   MIDI.setHandleNoteOff(handleNoteOff);
@@ -77,53 +72,8 @@ void setup() {
 
 }
 
-void set_prescaling (int midinote) {
-  cli(); // interrupts off
 
-  if (midinote < 11) {
-    // prescaling * 64
-    bitWrite(TCCR1B, CS10, 1);
-    bitWrite(TCCR1B, CS11, 1);
-    bitWrite(TCCR1B, CS12, 0);
-    //Serial.println("prescale64");
-  }
-  if (midinote > 10 && midinote < 47) {
-    // prescaling * 8
-    bitWrite(TCCR1B, CS10, 0);
-    bitWrite(TCCR1B, CS11, 1);
-    bitWrite(TCCR1B, CS12, 0);
-    //Serial.println("prescale8");
-  }
-  if (midinote > 46) {
-    // prescaling * 1
-    bitWrite(TCCR1B, CS10, 1);
-    bitWrite(TCCR1B, CS11, 0);
-    bitWrite(TCCR1B, CS12, 0);
-    //Serial.println("prescale1");
-  }
 
-  sei(); // interrupts on
-
-}
-
-void play_current_note( int current_note ){
-  float ratio;
-  int offset;
-
-  if( current_pitch_bend == 0 ) {
-    set_prescaling(current_note);
-    OCR1A = musical_freqs[current_note];
-  } 
-  else {
-    set_prescaling(current_note);
-    ratio = (float)current_pitch_bend/ MIDI_PITCHBEND_MAX;
-    if( current_note < 125 ) {
-      offset = int((musical_freqs[current_note+2] - musical_freqs[current_note]) * ratio);
-
-      OCR1A = musical_freqs[current_note] + offset;
-    }
-  }
-}
 
 
 void loop() {
@@ -133,32 +83,31 @@ void loop() {
 
 }
 
-
+//A Note-on event has been received
 void handleNoteOn(byte channel, byte pitch, byte velocity)
 {
-  // Do whatever you want when a note is pressed.
-  // Try to keep your callbacks short (no delays ect)
-  // otherwise it would slow down the loop() and have a bad impact
-  // on real-time performance.
+  // Pass the note onto the oscillators
+  OSC1.setNote(int(pitch));
 
-  current_note = pitch;
-  play_current_note(current_note);
-  digitalWrite( 13, HIGH );
 }
 
+// A Note-off event has been received
 void handleNoteOff(byte channel, byte pitch, byte velocity)
 {
   // Do something when the note is released.
   // Note that NoteOn messages with 0 velocity are interpreted as NoteOffs.
-  digitalWrite( 13, LOW );
+
 }
 
+// A pitch-bend event has been received:
 void handlePitchBend( byte channel, int bend )
 {
-  current_pitch_bend = bend;
-  play_current_note( current_note );
+  // Pass the pitchbend onto the oscillators
+  OSC1.setPitchBend(bend);
 
 }
+
+
 
 
 
---
+++ Oscillator.ino
@@ -0,0 +1,139 @@
+/***************************************************************************
+ * Mega Mini Synth
+ * 
+ * An Arduino Mega2560 based music synthesizer
+ * 
+ * See http://megaminisynth.blogspot.co.nz/ for details.
+ * 
+ * This file is used for the timer based oscillator class. This is what is used
+ * to generate the pulses for the waveshapers. The oscillator frequency is determined
+ * by incoming MIDI messages.
+ * 
+ * 
+ * D. Quentin McDonald
+ * dqmcdonald@gmail.com
+ * 2014
+ * 
+ ****************************************************************************/
+
+// frequencies for midi notes 0 - 127
+// This data and much of the code in this file is based on the work described in
+// http://www.sinneb.net/?p=161
+// The idea is that for certain frequency ranges different values of the prescaler of the 
+// timer will give more accurate results. Hence the values in this array are not increasing
+// monotonically as MIDI notes -10 use prescale 64, the next 35 using 8 and the rest with 1.
+// The method setPreScaling implements this.
+int musical_freqs[] = { 
+  15289, 14431, 13621, 12857, 12135, 11454, 10811, 10204, 9631, 9091, 8581, 64792, 61156,
+  57724, 54484, 51426, 48540, 45815, 43244, 40817, 38526, 36364, 34323, 32396, 30578, 28862,
+  27242, 25713, 24270, 22908, 21622, 20408, 19263, 18182, 17161, 16198, 15289, 14431, 13621,
+  12856, 12135, 11454, 10811, 10204, 9631, 9091, 8581, 64793, 61156, 57724, 54484, 51426, 48540,
+  45815, 43244, 40817, 38526, 36364, 34323, 32396, 30578, 28862, 27242, 25713, 24270, 22908, 21622,
+  20408, 19263, 18182, 17161, 16198, 15289, 14431, 13621, 12856, 12135, 11454, 10811, 10204, 9631, 9091,
+  8581, 8099, 7645, 7215, 6810, 6428, 6067, 5727, 5405, 5102, 4816, 4545, 4290, 4050, 3822, 3608, 3405,
+  3214, 3034, 2863, 2703, 2551, 2408, 2273, 2145, 2025, 1911, 1804, 1703, 1607, 1517, 1432, 1351, 1276,
+  1204, 1136, 1073, 1012, 956, 902, 851, 804, 758, 716, 676, 638 };
+
+
+// Set up an oscillator on the specified Timer
+
+Oscillator::Oscillator( int timer ) 
+{
+  m_note = 69; // A 440 Hz
+  m_pitch_bend = 0; // Start with no pitch bend
+
+  m_timer = timer;
+
+  if( m_timer == 1 ) {
+
+    // Timer1 is on pin 11
+    pinMode(TIMER1_OUT, OUTPUT);
+    // Timer/Counter 1 :: registers TCCR1A and TCCR1B
+
+    // Clear Timer on Compare Match (CTC) Mode
+    bitWrite(TCCR1A, WGM10, 0);
+    bitWrite(TCCR1A, WGM11, 0);
+    bitWrite(TCCR1B, WGM12, 1);
+    bitWrite(TCCR1B, WGM13, 0);
+
+    // Toggle OC1A on Compare Match. p.134
+    // arduino mega hw pin 11
+    bitWrite(TCCR1A, COM1A0, 1);
+    bitWrite(TCCR1A, COM1A1, 0);
+
+    // prescaling
+    setPreScaling(m_note);
+
+    // set output compare register A to curr_midinote (init: 69)
+    OCR1A = musical_freqs[m_note];
+
+  }
+}
+
+
+// Set the current MIDI note and update the oscillator to the corresponding frequency
+// How we do this depends on whether there is any pitch-bend in action.
+void Oscillator::setNote( int current_note )    
+{
+  m_note = current_note;
+  float ratio;
+  int offset;
+
+ setPreScaling(m_note);
+ 
+  if( m_pitch_bend == 0 ) { 
+    OCR1A = musical_freqs[m_note];
+  } 
+  else {
+    // Implement pitch bend. The MIDI standard suggests that the pitch bend is +/- two 
+    // semitones (or two MIDI notes). However because we use different pre-scalers for different
+    // note ranges we need to have some additional data to figure out the frequency.
+    // TODO - figure out how to handle discontiuity in frequency data
+    // and how to do a non-linear interpolation so it's more musicalß
+    ratio = (float)m_pitch_bend/ MIDI_PITCHBEND_MAX;
+    if( current_note < 125 ) {
+      offset = int((musical_freqs[m_note+2] - musical_freqs[m_note]) * ratio);
+      OCR1A = musical_freqs[m_note] + offset;
+    }
+  }
+} 
+
+// Set the amount of pitch bend that is currently being applied, then replay the current
+// note to enact that.
+void Oscillator::setPitchBend( int pitch_bend ) 
+{
+  m_pitch_bend = pitch_bend;
+  setNote( m_note );
+}
+
+
+void Oscillator::setPreScaling (int midinote) {
+  cli(); // interrupts off
+
+  if( m_timer == 1 ) {
+    if (midinote < 11) {
+      // prescaling * 64
+      bitWrite(TCCR1B, CS10, 1);
+      bitWrite(TCCR1B, CS11, 1);
+      bitWrite(TCCR1B, CS12, 0);
+    }
+    if (midinote > 10 && midinote < 47) {
+      // prescaling * 8
+      bitWrite(TCCR1B, CS10, 0);
+      bitWrite(TCCR1B, CS11, 1);
+      bitWrite(TCCR1B, CS12, 0);
+    }
+    if (midinote > 46) {
+      // prescaling * 1
+      bitWrite(TCCR1B, CS10, 1);
+      bitWrite(TCCR1B, CS11, 0);
+      bitWrite(TCCR1B, CS12, 0);
+      //Serial.println("prescale1");
+    }
+  }
+  sei(); // interrupts on
+
+}
+
+
+
